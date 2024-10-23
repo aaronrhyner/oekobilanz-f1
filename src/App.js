43192bf12b7adf39a820c681410a5b10a7441f8a
zuest <jonas.zuest@lernende.bbw.ch>
2024-10-23 09:05:33 +0200

    fixed routing

--- src/App.js
+++ src/App.js
@@ -1,47 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import BrowserRouter from "./components/Router";
 
-// Home Component
-const Home = () => {
-    return <h2>Home Page</h2>;
-};
-
-// About Component
-const About = () => {
-    return <h2>About Page</h2>;
-};
-
-// Contact Component
-const Contact = () => {
-    return <h2>Contact Page</h2>;
-};
 
 // App Component with Routing
 const App = () => {
     return (
-        <Router>
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/about">About</Link>
-                        </li>
-                        <li>
-                            <Link to="/contact">Contact</Link>
-                        </li>
-                    </ul>
-                </nav>
-
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/contact" element={<Contact />} />
-                </Routes>
-            </div>
-        </Router>
+        <BrowserRouter></BrowserRouter>
     );
 };
 
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,30 +1,21 @@
 import {BrowserRouter as Router, Link, Route, Routes} from "react-router-dom";
 import React from "react";
+import Home from "../pages/Home";
+import About from "../pages/About";
 
-const App = () => {
+
+const BrowserRouter = () => {
     return (
         <Router>
             <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/about">About</Link>
-                        </li>
-                        <li>
-                            <Link to="/contact">Contact</Link>
-                        </li>
-                    </ul>
-                </nav>
 
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/about" element={<About />} />
-                    <Route path="/contact" element={<Contact />} />
                 </Routes>
             </div>
         </Router>
     );
 };
+
+export default BrowserRouter;
